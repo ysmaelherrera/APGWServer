c2093987cd8d4adb19d1f2c90534505990768f9c
ysmaelherrera <104236695+ysmaelherrera@users.noreply.github.com>
2022-06-03 23:32:01 -0500

    Add files via upload

---
+++ src/main/java/idat/pe/APGWServer/ApgwServerApplication.java
@@ -0,0 +1,13 @@
+package idat.pe.APGWServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApgwServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApgwServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/idat/pe/APGWServer/ApgwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package idat.pe.APGWServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgwServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
